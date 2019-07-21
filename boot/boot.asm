01fe611ba167bbb1b64334cb1f0bdb65c1ab3723
D0ot <gbcays@qq.com>
2019-07-21 20:47:15 +0800

    1. change the kernel base address for memory detecting 2. add feature about memory detecting in boot.asm

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,5 +1,7 @@
-data_addr equ 0x7e00
-
+data_addr equ 0x7e00    ; base address
+meminfo_addr equ 0x8000 ; where to store ARDS first 4 Bytes is number of entries
+kernel_addr equ 0x10000 ; where should we load kerenl 
+ards_size equ 0x18      ; Address Range Descriptor Structure Size
 
 SECTION .mbrcheck
             db 0x55
@@ -13,10 +15,16 @@ gdt_base    dd 0x00007e00
         
 SECTION .text
 global _start
-_start:
+_start: 
             xor ax, ax
             mov ss, ax
+            mov es, ax 
             mov sp, 0x7c00
+            mov di, meminfo_addr
+            call _get_meminfo
+            
+            xor ax, ax
+            mov ss, ax
             mov bx, 0x7e00
             
             mov dword [bx + 0x00], 0
@@ -52,26 +60,30 @@ _start:
             mov es, ax
             mov esp, 0x7C00
 
-            mov ax, 0x01f
-            mov bx, 0x8000
-            mov cx, 0x1
+
+try_read_disk:
+            mov ax, 0x1F
+            mov ebx, kernel_addr 
+            mov ecx, 0x1
+            mov bp, 0
+            mov si, 0
+            mov di, 0
             call _disk_read
-
-
+            jc try_read_disk
 
             mov esp, 0x7c00
             mov ebp, 0x7c00
 
-            jmp 0x0008:0x8000
+            jmp 0x0008:kernel_addr
     _inf_loop_32bit:
             inc eax
             hlt
             jmp _inf_loop_32bit
 
 
-;   ax , number of logical sectors to read. 8 bits are valid
-;   bx , buffer to store data, it is an address
-;   cx , index of sector to read, 16 bits are valid
+;   ax , number of logical sectors to read. 8 bits are valid, 0 indicates 256 sectors
+;   ebx , buffer to store data, it is an address, 32bit are all valid 
+;   ecx , index of sector to read, lower 28 bits are valid
 
 _disk_read: 
             mov dx, 0x1F2
@@ -87,37 +99,121 @@ _disk_read:
             mov al, ah
             out dx, al          ; LBA 15 - 8
 
-            pop ax
-            mov cx, ax          ; now cx saves the number of sectors to read
+            shr ecx, 0x10       ; shift the ecx 16bits
 
             inc dx     
-            mov ax, 0x00
+            mov al, cl
             out dx, al          ; LBA 23 - 16
 
-            inc dx      
+            inc dx
+            and ch, 0x0F
             mov al, 0xE0        
+            or al, ch 
             out dx, al          ; LBA mode and LBA 27 - 24  
+
+            pop ax
+            mov cx, ax          ; now cx saves the number of sectors to read
 
             inc dx      
             mov al, 0x20
             out dx, al
 
-            xor edi, edi
-            mov di, bx
     dr_read_0:
             push cx
-            mov cx, 256 
             mov dx, 0x1f7
     dr_wait:
+            ;in al, dx
+            ;and al, 0x88
+            ;cmp al, 0x08
+            ;jnz dr_wait
+
             in al, dx
-            and al, 0x88
-            cmp al, 0x08
+            test al, 0x80
             jnz dr_wait
 
-            mov dx, 0x1F0
+            test al, 0x01
+            jnz dr_read_error_ret
             
-            rep insw 
+            test al, 0x08
+            jz dr_wait
+            
+            mov dx, 0x1F0
+            mov cx, 256 
+    dr_read_loop:
+            in ax, dx
+            mov [ebx], ax
+            add ebx, 2
+            loop dr_read_loop
+
             pop cx
             loop dr_read_0
 
+            clc
             ret
+    dr_read_error_ret:
+            stc
+            ret
+
+            [Bits 16]
+; \brief get memory infomation of the machine
+; \param es:di is buffer, first 4 bytes is numbre of entries in list, the following it is ARDS entries
+_get_meminfo:
+            xor bp, bp
+
+            mov eax, 0xe820
+            xor ebx, ebx
+            mov ecx, ards_size 
+            mov edx, 0x534d4150
+            mov es, bx
+            push di
+            add di, 4
+            mov [es:di + 20], dword 1       ; for ACPI 3.0 Extended Capibility, force it to be valid
+
+            clc                             ; for later error detect, clear carry flag
+            int 0x15
+            jc meminfo_failed               ; being stuck here indicates failure on reading memory info    
+
+            mov edx, 0x534d4150
+            cmp edx, eax
+            jne meminfo_failed              ; INT 0x15 returned eax is not 0x534d4150 'SMAP'
+            
+            cmp ebx, 0
+            je meminfo_failed               ; get ebx as 0, implies list is only 1 entry long, we see it as an error
+            jmp meminfo_entrycheck
+
+    meminfo_loop:
+            mov eax, 0xe820
+            mov [es:di + 20], dword 1
+            int 0x15
+            jc meminfo_success
+            mov edx, 0x534d4150
+            
+    meminfo_entrycheck:
+            jcxz meminfo_skip               ; ecx is zero, skip this entry
+            cmp cl, 20
+            jbe meminfo_notext
+            test byte [es:di + 20], 1
+            je meminfo_skip                 ; ACPI 3.0 Extended 
+
+    meminfo_notext:
+            mov ecx, [es:di + 8]            ; length_low bitwise_or length_high, if it is zero, it is not a vaild entry, skip it.
+            or ecx, [es:di + 12]
+            jz meminfo_skip
+            inc bp
+            add di, 24
+
+    meminfo_skip:
+            test ebx, ebx                   ; if ebx is 0, we reached the last entry
+            jne meminfo_loop
+
+    meminfo_success:
+            pop di
+            mov [di], bp
+            ret
+    meminfo_failed:
+            jmp meminfo_failed
+ 
+; if ecx is zero, skip it
+; if ebx is zero, end it
+; if, after int 15h , carry flag is set, end it
+            
