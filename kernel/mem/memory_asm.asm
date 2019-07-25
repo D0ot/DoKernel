7ed22759bab0f7db578e8886b35b22ee3d732d4a
D0ot <1306793135@qq.com>
2019-07-25 19:50:31 +0800

    how to use multi-segment in systeam. need to learn

--- kernel/mem/memory_asm.asm
+++ kernel/mem/memory_asm.asm
@@ -9,7 +9,18 @@ memory_switch_gdt:
 
 ; to use kernel segment
 ; flush the segment selector cache
+; 2 arguments, segment for kerenl
+; (uint16_t cs, uint16_t ds)
 memory_init_flush_0:
+    
+    mov ax, [esp + 0x08]
+    mov bx, [esp + 0x04]
+
+    mov ds, bx
+    mov es, bx
+    mov ss, bx
+
+.memory_init_flush_0_lab:
     
 
     ret
