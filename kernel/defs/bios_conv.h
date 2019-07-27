27a8eaec77e5cbc50b56baca333cfc44bc476879
D0ot <1306793135@qq.com>
2019-07-27 23:21:20 +0800

    misc commit. nothing important

--- kernel/defs/bios_conv.h
+++ kernel/defs/bios_conv.h
@@ -3,24 +3,25 @@
 
 
 #ifndef _BIOS_CONV_H 
-#define BIOS_CONV_H
+#define _BIOS_CONV_H
 
-#include "stdint.h"
+#include <stdint.h>
 #include "mem/memory.h"
+#include "defs/defs.h"
 
 // it is not a good design
 // TODO, using a more "smart" loader
 #define MAIN_PHYSICAL_MEMORY_START (0x100000) 
 
 // address to which boot.asm load the program 
-#define KERNEL_BASE (void*)(0x400000)
+#define KERNEL_BASE (void*)(KERNEL_ADDR)
 
 // not using ELF format, so hardcode the max image size
 // for learning purpose, 4Mib will be enough
 #define KERNEL_SIZE (uint32_t)(0x400000) 
 
 
-#define SMAP_BASE ((void*)0x8000)
+#define SMAP_BASE ((void*)MEMINFO_ADDR)
 #define SMAP_SIZE (*(uint32_t*)(SMAP_BASE))
 #define SMAP_ADDR ((Mem_SMAP_Entry*)(SMAP_BASE + 4))
 
---
+++ kernel/defs/defs.h
@@ -0,0 +1,12 @@
+#ifndef _DEFS_H
+#define _DEFS_H
+
+// content here is same with content in boot/defs/asm
+
+#define MEMINFO_ADDR (0x8000)
+#define KERNEL_ADDR (0x400000)
+#define ARDS_SIZE (0x18)
+#define LIB_ADDR (0x9000)
+
+
+#endif
